5478f0c0436a49aedc20da96d469e7156fc0a11f
Shimadakunn <lcombaret@hotmail.co.jp>
2024-10-13 01:21:02 +0200

    refactor: Add generic type to event handler in EventEmitter

--- front/utils/eventEmitter.ts
+++ front/utils/eventEmitter.ts
@@ -1,23 +1,24 @@
-type EventHandler = (...args: any[]) => void;
+// Define a generic type for the event handler
+type EventHandler<T extends unknown[] = unknown[]> = (...args: T) => void;
 
 class EventEmitter {
   private events: { [key: string]: EventHandler[] } = {};
 
-  on(event: string, handler: EventHandler) {
+  on<T extends unknown[]>(event: string, handler: EventHandler<T>) {
     if (!this.events[event]) {
       this.events[event] = [];
     }
-    this.events[event].push(handler);
+    this.events[event].push(handler as EventHandler);
   }
 
-  emit(event: string, ...args: any[]) {
+  emit<T extends unknown[]>(event: string, ...args: T) {
     const handlers = this.events[event];
     if (handlers) {
-      handlers.forEach((handler) => handler(...args));
+      handlers.forEach((handler) => (handler as EventHandler<T>)(...args));
     }
   }
 
-  off(event: string, handler: EventHandler) {
+  off<T extends unknown[]>(event: string, handler: EventHandler<T>) {
     const handlers = this.events[event];
     if (handlers) {
       this.events[event] = handlers.filter((h) => h !== handler);
